c19a08a1c56bdb79d2a6acfb0ca3cf2a516b34d5
astralvitz <astralvitz@pm.me>
2024-09-09 21:24:19 -0400

    use constants for scale and position, remove leva controls

--- portfolio-vite-react/src/sections/Hero.jsx
+++ portfolio-vite-react/src/sections/Hero.jsx
@@ -6,43 +6,6 @@ import HackerRoom from "../components/HackerRoom";
 import CanvasLoader from "../components/CanvasLoader";
 
 const Hero = () => {
-  const controls = useControls('Hacker Room', {
-    positionX: {
-      value: 2.5,
-      min: -10,
-      max: 10
-    },
-    positionY: {
-      value: 2.5,
-      min: -10,
-      max: 10
-    },
-    positionZ: {
-      value: 2.5,
-      min: -10,
-      max: 10
-    },
-    rotationX: { 
-      value: 0,
-      min: -10,
-      max: 10
-    },
-    rotationY: {
-      value: 0,
-      min: -10,
-      max: 10 
-    },
-    rotationZ: {
-      value: 0,
-      min: -10,
-      max: 10 
-    },
-    scale: {
-      value: 1,
-      min: 0.1,
-      max: 10 
-    }
-  });
   const isSmall = useMediaQuery({ maxWidth: 440 })
   const isMobile = useMediaQuery({ maxWidth: 768 })
   const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 })
@@ -59,7 +22,7 @@ const Hero = () => {
 
       <div className="w-full h-full absolute inset-0">
         {/* any element wrapped in Canvas inherits all three/fiber properties */}
-        <Leva />
+        {/* <Leva /> */}
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
@@ -67,9 +30,9 @@ const Hero = () => {
               // scale={0.1} 
               // position={[0, 0, 0]} 
               // rotation={[0, Math.PI, 0]}
-              position={[2, -8, 2]}
+              position={sizes.deskPosition}
               rotation={[0, Math.PI, 0]}
-              scale={isMobile ? controls.scale : 0.1}
+              scale={sizes.deskScale}
             />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} />
